d7da8f6250ac191ab6a6315abd7d6aae5f453dc2
fluderm <112969270+fluderm@users.noreply.github.com>
2023-03-27 13:52:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     include_package_data=True,
     package_data={"panda_gym": ["version.txt"]},
     version=__version__,
-    install_requires=["gym", "pybullet", "numpy", "scipy"],
+    install_requires=['setuptools==65.5.0',"gym", "pybullet", "numpy", "scipy"],
     extras_require={
         "tests": ["pytest", "black", "pytype"],
         "extra": ["numpngw", "stable-baselines3"],
